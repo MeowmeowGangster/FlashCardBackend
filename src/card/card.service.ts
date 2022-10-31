74217edcb94ff836be7914acaef418e570674f72
pattananNP <phattanun19@hotmail.com>
2022-10-31 18:58:49 +0700

    Feat: randcards

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -3,20 +3,20 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
-import { Card, CardDocument } from './schemas/card.schema'
+import { Card, CardDocument } from './schemas/card.schema';
 
 @Injectable()
 export class CardService {
   constructor(
     @InjectModel(Card.name) private readonly model: Model<CardDocument>,
-  ) { }
+  ) {}
 
   async findAll(): Promise<Card[]> {
     return await this.model.find().exec();
   }
 
   async findOne(id: string): Promise<Card> {
-    return await this.model.findOne({"cardID":id})
+    return await this.model.findOne({ cardID: id });
   }
 
   async create(createCardDto: CreateCardDto): Promise<Card> {
@@ -26,13 +26,22 @@ export class CardService {
   }
 
   async update(id: string, updateCardDto: UpdateCardDto): Promise<Card> {
-    return await this.model.findOneAndUpdate({ "cardID": id }, { $set: {
-      cardName:updateCardDto.cardName,
-      cardMemo:updateCardDto.cardMemo,
-      cardPic:updateCardDto.cardPic
-    } })
+    return await this.model.findOneAndUpdate(
+      { cardID: id },
+      {
+        $set: {
+          cardName: updateCardDto.cardName,
+          cardMemo: updateCardDto.cardMemo,
+          cardPic: updateCardDto.cardPic,
+        },
+      },
+    );
   }
   async delete(id: string): Promise<Card> {
-    return await this.model.findOneAndRemove({"cardID":id })
+    return await this.model.findOneAndRemove({ cardID: id });
+  }
+
+  async getRandomCard(number: number): Promise<Card[]> {
+    return await this.model.aggregate([{ $sample: { size: number } }]);
   }
 }
