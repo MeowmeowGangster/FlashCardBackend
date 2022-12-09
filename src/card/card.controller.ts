d40ded776078d491273b689c24eb2cc97cbf1194
pattananNP <phattanun19@hotmail.com>
2022-12-09 15:44:52 +0700

    fix: upload

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -8,6 +8,8 @@ import {
   Patch,
   UseGuards,
   Query,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { FirebaseAuthGuard } from 'src/auth/guard/firebase.guard';
 import { CardService } from './card.service';
@@ -15,10 +17,15 @@ import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { User } from 'src/auth/decorators/user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AssetsService } from './assets.service';
 
 @Controller()
 export class CardController {
-  constructor(private readonly cardService: CardService) {}
+  constructor(
+    private readonly cardService: CardService,
+    private readonly assestsServie: AssetsService,
+  ) {}
 
   @UseGuards(FirebaseAuthGuard)
   @Get()
@@ -32,11 +39,20 @@ export class CardController {
     return await this.cardService.getRandomCard(Number(limit));
   }
 
-  @UseGuards(FirebaseAuthGuard)
+  // @UseGuards(FirebaseAuthGuard)
   @Post('/cards')
-  async create(@User() user, @Body() createCardDto: CreateCardDto) {
-    createCardDto.ownerID = user.user_id;
+  @UseInterceptors(FileInterceptor('file')) // FileInterceptor is a NestJS interceptor that handles file uploads
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @User() user,
+    @Body() createCardDto: CreateCardDto,
+  ) {
+    createCardDto.ownerID = 'ddd';
     createCardDto.cardID = uuidv4();
+
+    const response = await this.assestsServie.updateAsset(file);
+
+    createCardDto.cardPic = response.url[0];
 
     return await this.cardService.create(createCardDto);
   }
